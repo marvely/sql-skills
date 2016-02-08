34ecd3e964b3123108ccd82b433530a7b0f5e80e
marvely <junezhangmj@gmail.com>
2016-02-08 15:30:46 -0800

    added missing ; in previous codes
    second part code included

--- advanced SQL 3.sql
+++ advanced SQL 3.sql
@@ -23,7 +23,7 @@ create table Starbucks_Employees
 	Name varchar(30),
 	Age integer,
 	HourlyRate  float,
-	StoreID integer)
+	StoreID integer) ;
 	
 Insert into Starbucks_Employees values
 	( 1, "Abe", 61, 14, 10),
@@ -31,7 +31,7 @@ Insert into Starbucks_Employees values
 	(5, "Chris", 34, 9, 40),
 	(7, "Dan", 41, 11, 50),
 	(8, "Ken", 57, 11, 60),
-	(11, "Joe", 38, 13, 70)
+	(11, "Joe", 38, 13, 70) ;
 	
 select * from Starbucks_Employees;
 
@@ -51,14 +51,80 @@ insert into Starbucks_Stores values
 select * from Starbucks_Stores;
 
 /* try the query*/
-select count(*) as num_employees, HourlyRate, city
+select count(*) as num_employees, max(HourlyRate), city
 from Starbucks_Employees se join Starbucks_Stores ss
 on se.StoreID = ss.store_id
-group by city, HourlyRate;
+group by city;
 
 /* the problem is the HourlyRate returned is not a reflection of the avg 
 of all the employees in the city
 */
 
+/* part 2 of the advanced SQL practice */
 
+/* find the largest order amount for each salesperson and the associated 
+order number, along with the customer to whom that order belongs to. 
+You can present your answer in any database’s SQL – MySQL, Microsoft 
+SQL Server, Oracle, etc.
+*/
+
+select max(Amount) as Max_Order_Amount, salesperson_id, Number as Order_number, c.Name as Customer_name
+from Orders o left outer join Customer_2 c on o.cust_id = c.ID
+group by salesperson_id;
+/* however, this order_number is randomly selected by SQL...*/
+/* we need to make our query more specific!!!!! */
+
+select salesperson_id, Max(Amount) as MaxOrder
+from Orders
+group by salesperson_id;
+
+/* if we join the subquery result with the order table we will have the specific order number 
+for the max(Amount) values
+*/
+select salesperson_id, Number as Order_Number, Amount
+from Orders join 
+	(select salesperson_id, Max(Amount) as MaxOrder
+	 from Orders
+	 group by salesperson_id) as TopOrderAmountsPerSalesperson
+using (salesperson_id)
+where Amount = MaxOrder;
+/* the next step, add customer's name is easy. */
+
+select salesperson_id, Number as Order_number, Amount, s.Name as Salesperson_name, c.Name as Cust_name
+from Orders o join salesperson s on s.ID = o.salesperson_id
+join Customer_2 c on c.ID = o.cust_id
+join (select salesperson_id, Max(Amount) as MaxOrder
+	 from Orders
+	 group by salesperson_id) as TopOrderAmountsPerSalesperson
+using (salesperson_id)
+where Amount = MaxOrder;
+
+/* corner case: if a salesperson has 2 or more orders that have the same value 
+for the highest amount????
+*/
+insert into Orders values
+	(80, "02/19/94", 7, 2, 2400) ;
+	
+/* and we run the query above one more time, have the result table with 2 rows of Bob's data.
+But the question is, if we only want 1 Bob, how to elliminate the duplicate?? */
+
+/* solution:
+				we can add group by amount, salesperson_id at the end of the query,
+				since the combination result is unique!!!!! */
+				
+select salesperson_id, Number as Order_number, Amount, s.Name as Salesperson_name, c.Name as Cust_name
+from Orders o join salesperson s on s.ID = o.salesperson_id
+join Customer_2 c on c.ID = o.cust_id
+join (select salesperson_id, Max(Amount) as MaxOrder
+	 from Orders
+	 group by salesperson_id) as TopOrderAmountsPerSalesperson
+using (salesperson_id)
+where Amount = MaxOrder
+group by Amount, salesperson_id;
+
+/* however, the order number for Bob will be random now!!!! is it ok??? */
+
+
+
+	
 /* end */
